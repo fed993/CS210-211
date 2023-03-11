843ef4517f3b101878d79cbb404edb01878c5ab0
Fed <Fed993@gmail.com>
2023-03-11 07:41:16 -0800

    updated comments assignment 4

--- Assignments/src/assignment_4/assignment04_FedPolyanskiy.java
+++ Assignments/src/assignment_4/assignment04_FedPolyanskiy.java
@@ -1,3 +1,14 @@
+/*
+ * CS210  (Assignment 4)
+ * Fedor Polyanskiy - f.polyanskiy@bellevuecollege.edu
+ * Student ID: 201 690 498
+ * 2023.03.10 - Winter Quarter
+ *
+ *	This main method creates an instance of People, and prompts People to 
+ *		list a few different lists of people's name, based on their occupation, gender, and age.
+ * 
+ */
+
 package assignment_4;
 
 import java.io.*;
